7a8b308cbe625654d173e1e3332ad2d88fd9725e
baont2003@gmail.com <baont2003@gmail.com@f765b241-9d93-97ea-eeb0-551a00319e68>
2014-09-12 12:43:00 +0000

    up

--- jwd-2014-class02-group10/src/group10/HelloWorld.java
+++ jwd-2014-class02-group10/src/group10/HelloWorld.java
@@ -4,8 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" Hello World");
+		System.out.println(" Hello World!!!!");
 		System.out.println("Success!!");
+    
 
 	}
 
